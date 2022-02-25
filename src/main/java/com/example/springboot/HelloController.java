54715ab6e9c0ac41f2dfae253dd8cdf1a692ef73
rsarabesh <rsarabesh@vmware.com>
2022-02-25 16:21:54 +0530

    changing to check config update

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings testing from Spring Boot + Tanzu!";
+		return "Greetings testing2 from Spring Boot + Tanzu!";
 	}
 
 }
